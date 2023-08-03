06b2ed86b401455234ccc3ff4f8e54f37a1fd549
Heta Björklund <heta.bjorklund@gmail.com>
2023-08-03 13:33:53 +0300

    Add letter function implementations

--- led/led.ino
+++ led/led.ino
@@ -1,5 +1,4 @@
 const int ledPin = 10;
-
 const int dotLenght = 1000;
 const int dashLength = 3000;
 const int spaceWithinLetter = 1000;
@@ -24,13 +23,16 @@ void loop() {
 
   for (char letter : message) {
     Serial.println(letter);
+
+    if (letter == ' ') {
+      getSpaceBetweenWords();
+    }
+    else {
+      getLetter(letter);
+    }       
+    getSpaceBetweenLetters();
   }
-  
-  digitalWrite(ledPin, HIGH);
-  delay(5000); // 5 seconds on
   digitalWrite(ledPin, LOW);
-  delay(2000); // 2 seconds off
-  
 }
 
 void getDot() {
@@ -56,6 +58,221 @@ void getSpaceBetweenLetters() {
 void getSpaceBetweenWords() {
   digitalWrite(ledPin, LOW);
   delay(spaceBetweenWords);
+}
+
+void getA() {
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+}
+
+void getB() {
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+}
+
+void getC() {
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+}
+
+void getD() {
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+}   
+
+void getE() {
+  getDot();
+}
+
+void getF() {
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+}
+
+void getG() {
+  getDash();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+}
+
+void getH() {
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+}
+
+void getI() {
+  getDot();
+  getSpaceWithInLetter();
+} 
+
+void getJ() {
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDash();
+}         
+
+void getK() {
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+}
+
+void getL() {
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+} 
+
+void getM() {
+  getDash();
+  getSpaceWithInLetter();
+  getDash();
+}
+
+void getN() {
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+}
+
+void getO() {
+  getDash();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDash();
+}
+
+void getP() {
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+}
+
+void getQ() {
+  getDash();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+}
+
+void getR() {
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+}
+
+void getS() {
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+} 
+
+void getT() {
+  getDash();
+}
+
+void getU() {
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+}
+
+void getV() {
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+}
+
+void getW() {
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDash();
+}
+
+void getX() {
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+}
+
+void getY() {
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDash();
+}
+
+void getZ() {
+  getDash();
+  getSpaceWithInLetter();
+  getDash();
+  getSpaceWithInLetter();
+  getDot();
+  getSpaceWithInLetter();
+  getDot();
 }
 
 void getLetter(char letter) {
@@ -112,56 +329,5 @@ void getLetter(char letter) {
         getY();
     case 'z':
         getZ();
+  }
 }
-
-void getA() {}
-
-void getB() {}
-
-void getC() {}
-
-void getD() {}   
-
-void getE() {}
-
-void getF() {}
-
-void getG() {}
-
-void getH() {}
-
-void getI() {} 
-
-void getJ() {}         
-
-void getK() {}
-
-void getL() {} 
-
-void getM() {}
-
-void getN() {}
-
-void getO() {}
-
-void getP() {}
-
-void getQ() {}
-
-void getR() {}
-
-void getS() {} 
-
-void getT() {}
-
-void getU() {}
-
-void getV() {}
-
-void getW() {}
-
-void getX() {}
-
-void getY() {}
-
-void getZ() {}
