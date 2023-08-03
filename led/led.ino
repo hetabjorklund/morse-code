8a05ba4ecb4200531cf0195226bb2d050702cf7a
Heta Björklund <heta.bjorklund@gmail.com>
2023-08-03 13:01:48 +0300

    Add skeleton functions for letters

--- led/led.ino
+++ led/led.ino
@@ -32,3 +32,124 @@ void loop() {
   delay(2000); // 2 seconds off
   
 }
+
+void getLetter(char letter) {
+  switch (letter) {
+    case 'a':
+        getA();
+    case 'b':
+        getB();
+    case 'c':
+        getC();
+    case 'd':
+        getD();                          
+    case 'e':   
+        getE();  
+    case 'f':
+        getF();
+    case 'g':
+        getG();
+    case 'h':
+        getH();
+    case 'i':
+        getI(); 
+    case 'j':   
+        getJ();         
+    case 'k':
+        getK();
+    case 'l':   
+        getL(); 
+    case 'm':   
+        getM();
+    case 'n':
+        getN();
+    case 'o':
+        getO();
+    case 'p':
+        getP();
+    case 'q':
+        getQ();
+    case 'r':   
+        getR();
+    case 's':   
+        getS(); 
+    case 't':   
+        getT();
+    case 'u':
+        getU();
+    case 'v':
+        getV();
+    case 'w':
+        getW();
+    case 'x':
+        getX();
+    case 'y':   
+        getY();
+    case 'z':
+        getZ();
+    case 'å':
+        getÅ();
+    case 'ä':
+        getÄ();
+    case 'ö':
+        getÖ();
+  }
+}
+
+void getA() {}
+
+void getB() {}
+
+void getC() {}
+
+void getD() {}   
+
+void getE() {}
+
+void getF() {}
+
+void getG() {}
+
+void getH() {}
+
+void getI() {} 
+
+void getJ() {}         
+
+void getK() {}
+
+void getL() {} 
+
+void getM() {}
+
+void getN() {}
+
+void getO() {}
+
+void getP() {}
+
+void getQ() {}
+
+void getR() {}
+
+void getS() {} 
+
+void getT() {}
+
+void getU() {}
+
+void getV() {}
+
+void getW() {}
+
+void getX() {}
+
+void getY() {}
+
+void getZ() {}
+
+void getÅ() {}
+
+void getÄ() {}
+
+void getÖ() {}
